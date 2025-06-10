ceda872da8d505252fee686d54930dcf376a105e
avu33 <annabeltvu@gmail.com>
2025-06-09 21:29:17 -0700

    added creds back

--- SQLurp_the_Pho/database/db_connector.py
+++ SQLurp_the_Pho/database/db_connector.py
@@ -2,9 +2,9 @@ import MySQLdb
 
 # Database credentials
 host = 'classmysql.engr.oregonstate.edu'    
-user = 'add_your_username_here'  # replace
-passwd = 'add_your_password_here'  # replace
-db = 'add_your_username_here'  # replace  
+user = 'cs340_vua3'       
+passwd = '0637'    
+db = 'cs340_vua3'   
 
 def connectDB(host = host, user = user, passwd = passwd, db = db):
     '''
